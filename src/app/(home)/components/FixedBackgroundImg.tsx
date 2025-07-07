b9de68f1806c28726c2ca37b09fb54c43958b064
Nerim <nerim5037@gmail.com>
2025-07-07 21:12:55 +0900

    fix: 랜딩페이지 프론트엔드 섹션 썸네일 위치 조정

--- src/app/(home)/components/FixedBackgroundImg.tsx
+++ src/app/(home)/components/FixedBackgroundImg.tsx
@@ -55,7 +55,7 @@ export default function FixedBackgroundImg({
                 "bg-pandamarket max-lg900:bg-[-40px] max-md:bg-[-90px] bg-low-height-pandamarket",
               id === 3 && "max-xs:bg-[-50px] bg-low-height-globalnomad",
               id === 4 && "bg-[50px] max-lg900:bg-[-40px] max-xs:bg-[-160px]",
-              id === 5 && "bg-low-height-rolling"
+              id === 5 && "max-xs:bg-[-120px] bg-low-height-rolling"
             )}
             style={{
               backgroundImage: `url(${thumbnailImg})`,
