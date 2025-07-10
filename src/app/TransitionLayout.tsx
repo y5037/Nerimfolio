14280106c9bdfbb0615b69f8b865e152a2940429
Nerim <nerim5037@gmail.com>
2025-07-10 22:16:47 +0900

    fix: 랜딩페이지 GPU 가속 유도 테스트 코드 삭제

--- src/app/TransitionLayout.tsx
+++ src/app/TransitionLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
 import clsx from "clsx";
@@ -11,19 +11,29 @@ export default function TransitionLayout({
   children: ReactNode;
 }) {
   const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const id = requestAnimationFrame(() => {
+      setIsMounted(true);
+    });
+    return () => cancelAnimationFrame(id);
+  }, []);
 
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{ duration: 0.3, ease: "easeOut" }}
-        className={clsx("min-h-full")}
-      >
-        {children}
-      </motion.div>
+      {isMounted && (
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3, ease: "easeOut" }}
+          className={clsx("min-h-full")}
+        >
+          {children}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
