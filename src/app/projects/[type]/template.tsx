79f1e089d1ce7b381f4abb87ad0f2183d3df1b3e
Nerim <nerim5037@gmail.com>
2025-07-22 17:17:51 +0900

    chore: 불필요한 파일 제거

--- src/app/projects/[type]/template.tsx
+++
@@ -1,6 +0,0 @@
-import TransitionLayout from "@/app/TransitionLayout";
-import { ReactNode } from "react";
-
-export default function Template({ children }: { children: ReactNode }) {
-  return <TransitionLayout>{children}</TransitionLayout>;
-}
