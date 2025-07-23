2d23f017143634aea4b6743dbb416433a90609ae
Nerim <nerim5037@gmail.com>
2025-07-23 14:12:08 +0900

    fix: 프론트엔드 섹션 2번째 프로젝트 협업 탭 비활성화

--- src/app/projects/[type]/[id]/components/frontend/components/MenuContainer.tsx
+++ src/app/projects/[type]/[id]/components/frontend/components/MenuContainer.tsx
@@ -4,11 +4,14 @@ import { useSideTabMenu } from "@/hooks/useSideTabMenu";
 import { frontendTabs } from "@/lib/constants/projectDetailTabs";
 import clsx from "clsx";
 import Image from "next/image";
+import { useParams } from "next/navigation";
 import { useState } from "react";
 
 export default function MenuContainer() {
   const [isActive, setIsActive] = useState("Overview");
   const { indicatorY, itemRefs } = useSideTabMenu(frontendTabs, isActive);
+
+  const params = useParams();
 
   return (
     <div className={clsx("sticky top-24 h-fit")}>
@@ -21,33 +24,37 @@ export default function MenuContainer() {
           )}
           style={{ transform: `translateY(${indicatorY}px)` }}
         />
-        {frontendTabs.map((tab, idx) => (
-          <li
-            key={idx}
-            ref={(el) => {
-              itemRefs.current[idx] = el;
-            }}
-            className={clsx(
-              "flex items-center text-gray100 font-light gap-2 pl-5 pr-12 py-3 rounded-xl cursor-pointer relative z-10",
-              "max-lg:flex-col max-lg:text-sm max-lg:pr-2 max-lg:pl-2",
-              "max-md:pl-3 max-md:pr-3"
-            )}
-            onClick={() => setIsActive(tab.title)}
-          >
-            <div
+        {frontendTabs.map((tab, idx) => {
+          if (params.id === "2" && idx === 3) return null;
+
+          return (
+            <li
+              key={idx}
+              ref={(el) => {
+                itemRefs.current[idx] = el;
+              }}
               className={clsx(
-                "relative w-[25px] h-[25px]",
-                "max-lg:w-[22px] max-lg:h-[22px]",
-                "max-md:w-[18px] max-md:h-[18px]"
+                "flex items-center text-gray100 font-light gap-2 pl-5 pr-12 py-3 rounded-xl cursor-pointer relative z-10",
+                "max-lg:flex-col max-lg:text-sm max-lg:pr-2 max-lg:pl-2",
+                "max-md:pl-3 max-md:pr-3"
               )}
+              onClick={() => setIsActive(tab.title)}
             >
-              <Image src={tab.icon} alt={tab.title} fill />
-            </div>
-            <p className={clsx("max-lg:text-sm", "max-md:hidden")}>
-              {tab.title}
-            </p>
-          </li>
-        ))}
+              <div
+                className={clsx(
+                  "relative w-[25px] h-[25px]",
+                  "max-lg:w-[22px] max-lg:h-[22px]",
+                  "max-md:w-[18px] max-md:h-[18px]"
+                )}
+              >
+                <Image src={tab.icon} alt={tab.title} fill />
+              </div>
+              <p className={clsx("max-lg:text-sm", "max-md:hidden")}>
+                {tab.title}
+              </p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
