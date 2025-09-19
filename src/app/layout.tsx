545a5778a97f79cbe5aac6e5aabec48f56a564ad
Nerim <nerim5037@gmail.com>
2025-09-19 20:10:21 +0900

    fix: 구글 서치 콘솔 색인 이슈 체크 - 메타 태그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <head>
+        <meta name="robots" content="index, follow" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no, viewport-fit=cover"
